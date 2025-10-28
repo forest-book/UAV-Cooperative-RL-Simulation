8ba3d529d7b464445477841de94f8eb74a1df294
forest-book <124020491+forest-book@users.noreply.github.com>
2025-10-28 19:23:18 +0900

    シミュレーションループを新たにmain.pyに作り直す

---
+++ main/main.py
@@ -0,0 +1,38 @@
+import numpy as np
+from collections import defaultdict
+from typing import List, Dict, Tuple
+
+from quadcopter import UAV, Senario
+from estimator import Estimator
+from data_handler import Plotter, DataLogger
+
+class MainController:
+    """アプリケーション全体を管理し，メインループを実行する"""
+    def __init__(self, params: dict):
+        self.params = params
+
+
+
+if __name__ == '__main__':
+    simulation_params = {
+        'T': 0.05,  # サンプリング周期 T
+        'GAMMA': 0, # ゲイン γ
+        'TARGET_ID': 1, # 推定目標
+        'EVENT': Senario.CONTINUOUS, #シナリオ選択
+        'INITIAL_POSITIONS': {
+            1: [0, 0], 2: [2, -30], 3: [20, -15],
+            4: [-20, 8], 5: [-14, 8], 6: [-10, -30]
+        },
+        'NEIGHBORS': { 
+            1: [],         #UAV1の隣接機(推定対象のため無し)
+            2: [1],        #UAV2の隣接機        
+            3: [1, 4, 5],  #UAV3の隣接機      
+            4: [1],        #UAV4の隣接機
+            5: [3, 4],     #UAV5の隣接機
+            6: [4]         #UAV6の隣接機
+        },
+        'NOISE': { 
+            'delta_bar': 0.5,
+            'dist_bound': 0.05
+        }
+    }
