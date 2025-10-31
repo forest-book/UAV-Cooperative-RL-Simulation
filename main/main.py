959631b453faeada6a3d96e84380000f03e782fc
forest-book <124020491+forest-book@users.noreply.github.com>
2025-10-31 19:37:04 +0900

    直接推定の実行まで

--- main/main.py
+++ main/main.py
@@ -57,14 +57,63 @@ class MainController:
         self.loop_amount = int(self.params['DURATION'] / self.params['T'])
         #print(f"step num: {self.loop_amount}")
 
+    def get_noisy_measurements(self, uav_i: UAV, uav_j: UAV) -> Tuple[np.ndarray, float, float]:
+        """
+        2UAV間の真の状態に基づき、ノイズが付加された測定値を生成する
+        シミュレータ上に測距モジュールがあるなら不要となる関数
+        """
+        # 真の相対値
+        true_x_ij = uav_j.true_position - uav_i.true_position
+        true_v_ij = uav_j.true_velocity - uav_i.true_velocity # 自機の速度情報はUWB RCM通信で送られてくる
+        true_d_ij = np.linalg.norm(true_x_ij) # UWBモジュールでの測距を模している
+        # 論文式(1)の上あたりの方程式から算出される
+        true_d_dot_ij = (true_x_ij @ true_v_ij) / (true_d_ij + 1e-9) # ゼロ除算防止
+
+        # ノイズモデル (4.1節)
+        delta_bar = self.params['NOISE']['delta_bar']
+        dist_bound = self.params['NOISE']['dist_bound']
+        
+        # 速度ノイズ: [-δ̄/2, δ̄/2] の一様乱数
+        vel_noise = np.random.uniform(-delta_bar / 2, delta_bar / 2, size=2)
+        # 距離ノイズ: [-bound/2, bound/2] の一様乱数
+        dist_noise = np.random.uniform(-dist_bound / 2, dist_bound / 2)
+        # 距離変化率ノイズ: [-bound/2, bound/2] の一様乱数
+        dist_rate_noise = np.random.uniform(-dist_bound / 2, dist_bound / 2)
+
+        return true_v_ij, true_d_ij, true_d_dot_ij
+
     def run(self):
         """メインループの実行"""
         self.initialize()
 
         #for loop in self.loop_amount:
-        for loop in range(5): #5ループでのデバッグ用
+        for loop in range(1): #5ループでのデバッグ用
             
-            
+            # 直接推定の実行
+            for uav_i in self.uavs:
+                # 各UAVが自機のすべての隣接機に対して行う
+                print(f"uav_{uav_i.id}")
+                for neighbor_id in uav_i.neighbors:
+                    neighbor_uav = self.uavs[neighbor_id - 1]
+                    print(f"uav_{uav_i.id}_{neighbor_id}")
+                    # ノイズ付き観測値を取得
+                    noisy_v, noisy_d, noisy_d_dot = self.get_noisy_measurements(uav_i, neighbor_uav)
+                    print(f"相対速度: {noisy_v}")
+                    print(f"距離: {noisy_d}")
+                    print(f"距離の変化率: {noisy_d_dot}")
+                    # 式(1)の計算
+                    next_direct = self.estimator.calc_direct_RL_estimate(
+                        chi_hat_ij_i_k=uav_i.direct_estimates[neighbor_id],
+                        noisy_v=noisy_v,
+                        noisy_d=noisy_d,
+                        noisy_d_dot=noisy_d_dot,
+                        T=self.dt,
+                        gamma=self.params['GAMMA']
+                    )
+                    print(f"直接推定値: {next_direct}")
+                    print("-"*50)
+                print("="*50)
+
 
 
 
